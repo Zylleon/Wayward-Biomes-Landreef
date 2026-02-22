3aa81ed9a185cfdde850e163c0c265f41abad2bc
Zylleon <56230821+Zylleon@users.noreply.github.com>
2026-02-22 09:54:54 -0700

    new Gatherable comp, coiled bitte "wool growth" now reads "silk production"

---
+++ Source/ZWB_Tidewrack/CompGatherable.cs
@@ -0,0 +1,70 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace Tidewrack
+{
+    public class CompGatherable : CompHasGatherableBodyResource
+    {
+        protected override int GatherResourcesIntervalDays => Props.gatherIntervalDays;
+
+        protected override int ResourceAmount => Props.itemAmount;
+
+        protected override ThingDef ResourceDef => Props.itemDef;
+
+        protected override string SaveKey => "woolGrowth";
+
+        public CompProperties_Gatherable Props => (CompProperties_Gatherable)props;
+
+        protected override bool Active
+        {
+            get
+            {
+                if (!base.Active)
+                {
+                    return false;
+                }
+                Pawn pawn = parent as Pawn;
+                if (pawn != null && !pawn.ageTracker.CurLifeStage.shearable)
+                {
+                    return false;
+                }
+                if (ModsConfig.AnomalyActive && pawn.IsShambler)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public override string CompInspectStringExtra()
+        {
+            if (!Active)
+            {
+                return null;
+            }
+            return Props.growthLabel + ": " + base.Fullness.ToStringPercent();
+        }
+    }
+
+    public class CompProperties_Gatherable : CompProperties
+    {
+        public int gatherIntervalDays;
+
+        public int itemAmount = 1;
+
+        public ThingDef itemDef;
+
+        public string growthLabel;
+
+        public CompProperties_Gatherable()
+        {
+            compClass = typeof(CompGatherable);
+        }
+    }
+}
