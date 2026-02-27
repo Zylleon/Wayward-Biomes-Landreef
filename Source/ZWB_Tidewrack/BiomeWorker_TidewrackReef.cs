d949e7467890d5a6ff02568ef13b03de180a3664
Zylleon <56230821+Zylleon@users.noreply.github.com>
2026-02-26 18:38:47 -0700

    sumpter snail graphics, biomeworker

---
+++ Source/ZWB_Tidewrack/BiomeWorker_TidewrackReef.cs
@@ -0,0 +1,50 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld.Planet;
+
+namespace Tidewrack
+{
+    public class BiomeWorker_TidewrackReef : BiomeWorker
+    {
+        public override float GetScore(BiomeDef biome, Tile tile, PlanetTile planetTile)
+        {
+            if (tile.WaterCovered)
+            {
+                return -100f;
+            }
+            if (tile.temperature < -8f)
+            {
+                return 0f;
+            }
+            if (tile.elevation > 300f)
+            {
+                return 0f;
+            }
+            if (tile.rainfall < 800f)
+            {
+                return 0f;
+            }
+            if (tile.rainfall > 2200f)
+            {
+                return 0f;
+            }
+            if (tile.swampiness > 0.3f || tile.swampiness < 0.0f)
+            {
+                return 0f;
+            }
+            if (tile.swampiness > 0.11f && tile.swampiness < 0.18f)
+            {
+                return 0f;
+            }
+
+            return 37 - (tile.elevation / 7f) + 2f * (tile.temperature - 18f);
+
+        }
+
+    }
+
+}
